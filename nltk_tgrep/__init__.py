1c6a6467c4f657d2bc1864008f4244cf5d822811
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-02-18 14:21:20 +0100

    __init__: read in VERSION

--- nltk_tgrep/__init__.py
+++ nltk_tgrep/__init__.py
@@ -6,3 +6,18 @@ TGrep search implementation for NTLK trees.
 
 (c) 16 March, 2013 Will Roberts <wildwilhelm@gmail.com>.
 '''
+
+from codecs import open
+from os import path
+
+# Version. For each new release, the version number should be updated
+# in the file VERSION.
+try:
+    # If a VERSION file exists, use it!
+    with open(path.join(path.dirname(__file__), 'VERSION'),
+              encoding='utf-8') as infile:
+        __version__ = infile.read().strip()
+except NameError:
+    __version__ = 'unknown (running code interactively?)'
+except IOError as ex:
+    __version__ = "unknown (%s)" % ex
